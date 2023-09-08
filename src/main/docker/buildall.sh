07d144840af0f89d30210081dce527d64a64cd5f
Bear Giles <bgiles@coyotesong.com>
2023-09-08 13:54:43 -0600

    Fixed push versions?...

--- src/main/docker/buildall.sh
+++ src/main/docker/buildall.sh
@@ -2,6 +2,8 @@
 
 # this should be determined at runtime!
 
+PG_VERSION_16=16rc1   # postgresql-16-pljava does not yet exist
+
 PG_VERSION_15=15.4
 PG_VERSION_14=14.9
 # PG_VERSION_13=13.12
@@ -15,10 +17,12 @@ for release in bookworm bullseye
 do
   for tag in pgxnclient pljava
   do
+    # docker build --build-arg PG_VERSION=${PG_VERSION_16} --build-arg PG_MAJOR=16 --build-arg RELEASE=${release} --target ${tag} --tag beargiles/postgres-${tag}:${PG_VERSION_16}-${release} .
     docker build --build-arg PG_VERSION=${PG_VERSION_15} --build-arg PG_MAJOR=15 --build-arg RELEASE=${release} --target ${tag} --tag beargiles/postgres-${tag}:${PG_VERSION_15}-${release} .
     docker build --build-arg PG_VERSION=${PG_VERSION_14} --build-arg PG_MAJOR=14 --build-arg RELEASE=${release} --target ${tag} --tag beargiles/postgres-${tag}:${PG_VERSION_14}-${release} .
   done
 
+  # docker build --build-arg PG_VERSION=${PG_VERSION_16} --build-arg PG_MAJOR=16 --build-arg RELEASE=${release} --target pljavadev --tag beargiles/postgres-pljava-dev:${PG_VERSION_16}-${release} .
   docker build --build-arg PG_VERSION=${PG_VERSION_15} --build-arg PG_MAJOR=15 --build-arg RELEASE=${release} --target pljavadev --tag beargiles/postgres-pljava-dev:${PG_VERSION_15}-${release} .
   docker build --build-arg PG_VERSION=${PG_VERSION_14} --build-arg PG_MAJOR=14 --build-arg RELEASE=${release} --target pljavadev --tag beargiles/postgres-pljava-dev:${PG_VERSION_14}-${release} .
 done
@@ -30,12 +34,17 @@ for release in bookworm bullseye
 do
   for tag in pgxnclient pljava pljava-dev
   do
+    # docker tag beargiles/postgres-${tag}:${PG_VERSION_16}-${release} beargiles/postgres-${tag}:16rc1-${release}
     docker tag beargiles/postgres-${tag}:${PG_VERSION_15}-${release} beargiles/postgres-${tag}:15-${release}
-    docker tag beargiles/postgres-${tag}:${PG_VERSION_15}-${release} beargiles/postgres-${tag}:15
-
     docker tag beargiles/postgres-${tag}:${PG_VERSION_14}-${release} beargiles/postgres-${tag}:14-${release}
-    docker tag beargiles/postgres-${tag}:${PG_VERSION_15}-${release} beargiles/postgres-${tag}:14
   done
+done
+
+for tag in pgxnclient pljava pljava-dev
+do
+  # docker tag beargiles/postgres-${tag}:${PG_VERSION_16}-${release} beargiles/postgres-${tag}:16rc1
+  docker tag beargiles/postgres-${tag}:${PG_VERSION_15}-bookworm beargiles/postgres-${tag}:15
+  docker tag beargiles/postgres-${tag}:${PG_VERSION_14}-bookworm beargiles/postgres-${tag}:14
 done
 
 #
